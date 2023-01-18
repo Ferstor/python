8a02db0d766d087b3311284001414fce72f14ea2
Ferstor <113024863+Ferstor@users.noreply.github.com>
2023-01-18 15:44:44 +0300

    Update simplicity.py

--- simplicity.py
+++ simplicity.py
@@ -7,11 +7,17 @@ def is_simple(a):
         return simple_dict.get(a)
 
     sqrta = int(math.sqrt(a))
+
+    if a == 0:
+        return 'Not prime or comosite'
+
+    if a == 1:
+        return 'Not prime or comosite'
+    
     for i in range(2,sqrta):
         if a % i == 0:
-            simple_dict[a] = False
-            return False
-    simple_dict[a] = True
-    return True
-
-print(is_simple(100))
+            simple_dict[a] = 'Composite'
+            return 'Composite' 
+        
+    simple_dict[a] = 'Prime'
+    return 'Prime'
